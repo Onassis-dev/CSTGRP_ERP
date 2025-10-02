7d63f9a8365351f7e3031cbdaee8a1dcfa4d54cb
Onassis <onassis.dev@gmail.com>
2025-10-02 12:51:06 -0700

    fix: update barcode generation logic to handle 12-digit codes correctly

--- apps/api/src/routes/Resources/labels/labels.create.ts
+++ apps/api/src/routes/Resources/labels/labels.create.ts
@@ -322,12 +322,19 @@ export function createLabel(
 
   if (type === 'codigo-yamaha') {
     const barcode1Canvas = createCanvas(0, 0);
-    JsBarcode(barcode1Canvas, info.code.replace(/-/g, ''), {
-      format: 'CODE39',
-      width: 2,
-      height: 100,
-      displayValue: false,
-    });
+    JsBarcode(
+      barcode1Canvas,
+      (info.code.length === 12 ? info.code + '-00' : info.code).replace(
+        /-/g,
+        '',
+      ),
+      {
+        format: 'CODE39',
+        width: 2,
+        height: 100,
+        displayValue: false,
+      },
+    );
 
     const barcode2Canvas = createCanvas(0, 0);
     JsBarcode(barcode2Canvas, '1', {
