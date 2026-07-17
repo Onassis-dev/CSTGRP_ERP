92758f64dd38e110ed3530876bad05ee91c1596e
Onassis <onassis.dev@gmail.com>
2026-07-17 12:37:24 -0700

    feat: add copy password functionality with clipboard support in pw-cell component

--- apps/app/src/lib/components/ui/table/pw-cell.svelte
+++ apps/app/src/lib/components/ui/table/pw-cell.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import type { HTMLTdAttributes } from 'svelte/elements';
 	import { cn } from '$lib/utils.js';
+	import { CopyIcon } from 'lucide-svelte';
+	import { showSuccess } from '$lib/utils/showToast';
 
 	type $$Props = HTMLTdAttributes & {
 		password?: string;
@@ -18,15 +20,29 @@
 </script>
 
 <td
-	class={cn('h-[35px] border-r px-3 align-middle', className)}
+	class={cn(
+		'flex h-[35px] items-center justify-center gap-2 border-r px-3 align-middle',
+		className
+	)}
 	{...rest}
 	onclick={() => (show = !show)}
 >
 	{#if password}
+		<button
+			class="flex h-full w-full items-center justify-center"
+			onclick={(e) => {
+				e.preventDefault();
+				e.stopPropagation();
+				navigator.clipboard.writeText(password);
+				showSuccess('Copiado');
+			}}
+		>
+			<CopyIcon class="h-4 w-4" />
+		</button>
 		{#if show}
 			{password}
 		{:else}
-			<span class="text-sm">●●●●●●●●●●●●●●●</span>
+			<span class="text-sm">●●●●●●●●●</span>
 		{/if}
 	{/if}
 </td>
